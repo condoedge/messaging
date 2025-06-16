f4dcf8ec0b53f89f16ab241863c3c59de4fbb5cb
Ezequiel <tavelliezequiel@gmail.com>
2025-06-16 15:35:46 -0300

    Minor fix

--- src/Kompo/CustomInbox/ThreadForm.php
+++ src/Kompo/CustomInbox/ThreadForm.php
@@ -4,7 +4,7 @@ namespace Condoedge\Messaging\Kompo\CustomInbox;
 
 use App\Models\Messaging\Message;
 use App\Models\Messaging\Thread;
-use Condoedge\Utils\Kompo\Common\Form;
+use Condoedge\Utils\Facades\FileModel;
 
 class ThreadForm extends Form
 {
@@ -179,7 +179,7 @@ class ThreadForm extends Form
 			'recipients' => $this->threadId ? '' : 'required_without:massive_recipients_group',
 			'subject' => $this->threadId ? '' : 'required|max:1000',
 			'html' => 'required_without:attachments',
-		], File::attachmentsRules());
+		], FileModel::attachmentsRules());
 	}
 
 }
